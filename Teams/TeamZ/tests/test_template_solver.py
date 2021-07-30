a447d9c943ae2ff2f788829826aa147a2681c68a
zypto <55851811+zypto-cloud@users.noreply.github.com>
2021-07-30 09:57:30 +0200

    copied and renamed template

---
+++ Teams/TeamZ/tests/test_template_solver.py
@@ -0,0 +1,17 @@
+
+# import unittest
+import sys
+import os
+# import pytest
+
+sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+from TeamZAlgo import TeamZAlgo
+
+mg = TeamZAlgo()
+
+
+# class FillMazeTest(unittest.TestCase):
+#     def test_isingrid(self):
+#        self.assertTrue(mg.loadMaze(os.path.realpath(os.path.dirname(
+#            __file__))+"/../../../MazeExamples/maze1.txt"))
+#        self.assertTrue(mg.isInGrid(0, 0))
